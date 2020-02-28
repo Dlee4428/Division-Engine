5de8565ef0fd40a15dc2b37549caaaf6e8597e51
Dong Hyeong Lee <44447609+Dlee4428@users.noreply.github.com>
2020-02-28 00:08:23 -0500

    Skybox Changes

--- Division-Engine/Resources/Shaders/skyboxFrag.fs
+++ Division-Engine/Resources/Shaders/skyboxFrag.fs
@@ -1,13 +1,20 @@
 #version 450 core
 
-uniform samplerCube sampler;
-
 in VertexShaderOut {
-	vec3 texCoord;
+	vec3 texCoords;
 } fs_in;
 
-out vec4 color;
+out vec4 fragColor;
+
+uniform samplerCube cubeMap;
 
 void main() {
-	color = texture(sampler, fs_in.texCoord);
+	vec4 envColor = texture(cubeMap, fs_in.texCoords);
+	float lowerLimit = 0.3;
+	float upperLimit = 0.1;
+	vec3 skyColor = vec3(0.6, 0.6, 0.6);
+	float factor = (fs_in.texCoords.y - lowerLimit) / (upperLimit - lowerLimit);
+	factor = clamp(factor, 0.0, 1.0);
+
+	fragColor = mix(vec4(skyColor, 1.0), envColor, factor);
 }
--- Division-Engine/Resources/Shaders/skyboxVert.vs
+++ Division-Engine/Resources/Shaders/skyboxVert.vs
@@ -1,14 +1,14 @@
 #version 450 core
 
-layout(location = 0) in vec3 position;
+layout(location = 0) in vec3 aPos;
 layout(location = 3) uniform mat4 mvp;
 
 out VertexShaderOut {
-	vec3 texCoord;
+	vec3 texCoords;
 } vs_out;
 
 
 void main() {
-	vs_out.texCoord = vec3(position.x, -position.y, position.z);
-	gl_Position = mvp * vec4(position, 1.0);
+	vs_out.texCoords = vec3(aPos.x, -aPos.y, aPos.z);
+	gl_Position = mvp * vec4(aPos, 1.0);
 }
