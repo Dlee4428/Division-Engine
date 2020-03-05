67f5b4cdf416b2ae2a3e65f3f5e729017468dbfe
Dong Hyeong Lee <44447609+Dlee4428@users.noreply.github.com>
2020-03-05 16:59:18 -0500

    fragment shader partial done

--- Division-Engine/Resources/Shaders/terrainShader.fs
+++ Division-Engine/Resources/Shaders/terrainShader.fs
@@ -33,3 +33,48 @@ out TerrainGSOut {
 
 out vec4 fragColor;
 
+// PARAM - Y VERT POS, THRESHHOLD FOR MULTITEXTURE HEIGHT BASED RENDERING
+vec4 BlendTextureAlbedo(float vert_, thres1_, thres2_, thres3_, float transition_) {
+	// BLENDING TEXTURE IF ELSE STATEMENT TO CAP WHERE TO BLEND EACH STAGE OF HEIGHT
+	// THRESHOLD STAGE 1
+	if (vert_ < thres1_ - transition_) {
+		return texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 0.0);
+	}
+	else if (vert_ < thres1_ + transition_) {
+		float factor1 = (vert_ - (thres1_ - transition_)) / (2.0 * transition_);
+		return mix(
+			texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 0.0)), 
+			texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 1.0)), 
+			factor1);
+	}
+	// THRESHOLD STAGE 2
+	else if (vert_ < thres2_ - transition_) {
+		return texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 1.0));
+	}
+	else if (vert_ < thres2_ + transition_) {
+		float factor2 = (vert_ - (thres2_ - transition_)) / (2.0 * transition_);
+		return mix(
+			texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 1.0)), 
+			texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 2.0)), 
+			factor2);
+	}
+	// THRESHOLD STAGE 3
+	else if (vert_ < thres3_ - transition_) {
+		return texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 2.0));
+	}
+	else if (vert_ < thres3_ + transition_) {
+		float factor3 = (vert_ - (thres3_ - transition_)) / (2.0 * transition_);
+		return mix(
+			texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 2.0)), 
+			texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 3.0)), 
+			factor3);
+	}
+	else {
+		return texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 3.0));
+	}
+}
+
+void main() {
+
+}
+
