61accce2806b3d9a18570f6369d16e95948c799f
Dong Hyeong Lee <44447609+Dlee4428@users.noreply.github.com>
2020-03-05 21:22:15 -0500

    fragment Shader

--- Division-Engine/Resources/Shaders/terrainShader.fs
+++ Division-Engine/Resources/Shaders/terrainShader.fs
@@ -14,8 +14,8 @@ layout(binding = 3) uniform sampler2DShadow shadowMapping;
 
 // UNIFORM LOCATION FOR TERRAIN FRAGMENT SHADER
 layout(location = 13) uniform mat4 shadowMat;
-layout(location = 20) uniform vec3 DirectionalLight; // Sun Direction
-layout(location = 21) uniform vec3 DirectionalLightColor; // Sun Light Color
+layout(location = 20) uniform vec3 directionalLight; // Sun Direction
+layout(location = 21) uniform vec3 directionalLightColor; // Sun Light Color
 layout(location = 32) uniform float scaleDisplacement;
 layout(location = 35) uniform bool isFogEnable;
 layout(location = 36) uniform bool wireFrameMode;
@@ -32,6 +32,7 @@ out TerrainGSOut {
 } fs_in;
 
 out vec4 fragColor;
+float shadowFactor = 1.0;
 
 // PARAM - Y VERT POS, THRESHHOLD FOR MULTITEXTURE HEIGHT BASED RENDERING
 vec4 BlendTextureAlbedo(float vert_, thres1_, thres2_, thres3_, float transition_) {
@@ -75,6 +76,71 @@ vec4 BlendTextureAlbedo(float vert_, thres1_, thres2_, thres3_, float transition
 }
 
 void main() {
+	
+	if (isShadowMapping) {
+		// TERRAIN SHADOW MAPPING
+		// textureProjOffset divides each coordinate component by their last value, 
+		// and it is performing perspective division
+		// the resulting 3rd component is used in the depth comparison 
+		// (because its a shadow sampler with depth comparison)
+		// 4 texels sampled diagonally. If filtering is enabled this yields 16 texels
+		float result = 0;
+		result += textureProjOffset(shadowMapping, fs_in.shadowMappingPos, ivec2(-1, -1));
+		result += textureProjOffset(shadowMapping, fs_in.shadowMappingPos, ivec2( 1, -1));
+		result += textureProjOffset(shadowMapping, fs_in.shadowMappingPos, ivec2(-1,  1));
+		result += textureProjOffset(shadowMapping, fs_in.shadowMappingPos, ivec2( 1,  1));
+		shadowFactor = result * 0.25;
+	}
 
+	// TERRAIN LIGHTING
+	vec3 norm = normalize((texture(textureNorm, fs_in.texCoordNorm).rgb * 2.0) - 0.8);
+	vec3 ambient = directionalLightColor * 0.15;
+	vec3 diffuse = directionalLightColor * max(0.0, dot(norm, -directionalLight));
+	vec3 light = ambient + shadowFactor * diffuse;
+
+	// HEIGHT BASED MULTILAYER TEXTURE
+	float height = fs_in.worldPos;
+	const float thres1 = scaleDisplacement * 0.15;
+	const float thres2 = scaleDisplacement * 0.45;
+	const float thres3 = scaleDisplacement * 0.9;
+	const float transition = scaleDisplacement * 0.1;
+
+	vec4 albedoTex = BlendTextureAlbedo(height, thres1, thre2, thre3, transition);
+	float texSlope = 1.0 - norm.y;
+	albedoTex = mix(albedoTex, texture(textureAlbedo, vec3(fs_in.texCoordAlbedo, 3.0)), texSlope * 0.15);
+	
+	fragColor = vec4(light, 1.0) * albedoTex;
+
+	// WIREFRAME MODE
+	if (wireFrameMode) {
+		const vec4 wireframeColor = vec4(0.0, 0.0, 0.0, 0.0);
+		const float wireframeLineSize = 1.0;
+		float wireframeStrength = 0.0;
+
+		float fDistToEdge = min(fs_in.gEdgeDistance.x, min(fs_in.gEdgeDistance.y, fs_in.gEdgeDistance.z));
+		if (fDistToEdge <= wireframeLineSize) {
+			wireframeStrength = smoothstep(0.0, 1.0, 1.0 - (fDistToEdge / wireframeLineSize));
+		}
+		fragColor = wireframeColor * wireframeStrength + (1.0 - wireframeStrength) * fragColor;
+	}
+
+	// FOG MODE
+	if (isFogEnable) {
+		// Compute range based distance
+		float dist = length(fs_in.eyePos);
+
+		// Fog factors
+		// smoothstep — perform Hermite interpolation between two values
+		// smoothstep performs smooth Hermite interpolation between 0 and 1 when edge0 < x < edge1. 
+		// This is useful in cases where a threshold function with a smooth transition is desired.
+		// Source - https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/smoothstep.xhtml
+		float be = 0.0005 * smoothstep(0.0, scaleDisplacement, scaleDisplacement - fs_in.worldPos.y);
+		float bi = 0.0002 * smoothstep(0.0, scaleDisplacement, scaleDisplacement - fs_in.worldPos.y);
+		
+		float ext = exp(-dist * be); // extintion
+		float insc = exp(-dist * bi); // inscattering
+		const vec4 fogColor = vec4(0.7, 0.8, 0.9, 1.0);
+		fragColor = fragColor * ext + fogColor * (1.0 - insc);
+	}
 }
 
